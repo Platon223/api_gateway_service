e0b8c7537dc66637e1df63b1a28640bab5a90653
Platon <platon.tikhnenko@gmail.com>
2025-10-18 09:47:27 -0700

    Changed the gates auth modules into one reusable auth_route function and registered some routes from the gateway to auth service

--- blueprints/auth/routes.py
+++ blueprints/auth/routes.py
@@ -1,17 +1,27 @@
 from flask import Flask, request, jsonify, Blueprint
 from flask_jwt_extended import jwt_required
-from gates.auth.auth_login import auth_login
-from gates.auth.auth_register import auth_register
+from gates.auth.auth_route import auth_route
 
 gate_to_auth_bl = Blueprint('gate_to_auth', __name__)
 
 @gate_to_auth_bl.route('/login', methods=['POST'])
 def login():
-    return auth_login()
+    return auth_route('login?step=first_entry')
 
 @gate_to_auth_bl.route('/register', methods=['POST'])
 def register():
-    return auth_register()
+    return auth_route('register')
+
+@gate_to_auth_bl.route('/verify', methods=['POST'])
+def verify():
+    return auth_route('verify')
+
+@gate_to_auth_bl.route('/refresh', methods=['POST'])
+@jwt_required(refresh=True)
+def refresh():
+    return auth_route('refresh')
+
+
 
 
 
