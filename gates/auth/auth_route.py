eac47cea367d55379233da5ac1cc6482056737a1
Platon <platon.tikhnenko@gmail.com>
2025-10-18 13:15:31 -0700

    Provided the refresh token in the request when calling the refresh route in auth

--- gates/auth/auth_route.py
+++ gates/auth/auth_route.py
@@ -6,8 +6,14 @@ import requests
 load_dotenv()
 
 def auth_route(service_route):
-    json_data = request.get_json()
-    req = requests.post(f"{os.getenv('AUTH_SERVICE_URL')}/auth/{service_route}", json=json_data)
+    if service_route != 'refresh':
+
+        json_data = request.get_json()
+        req = requests.post(f"{os.getenv('AUTH_SERVICE_URL')}/auth/{service_route}", json=json_data)
+    else:
+        rftk_header = request.headers.get('Authorization')
+        rftk = rftk_header.split(" ")[1]
+        req = requests.post(f"{os.getenv('AUTH_SERVICE_URL')}/auth/{service_route}", headers={'Authorization': rftk})
 
     if req.status_code == 200:
         return Response(
