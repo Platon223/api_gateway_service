36a4f719275035b18cabf55409df9dc7e6a6ff7e
Platon <platon.tikhnenko@gmail.com>
2025-11-16 19:45:09 -0800

    started the first community route on gateway

--- blueprints/community/routes.py
+++ blueprints/community/routes.py
@@ -1,3 +1,10 @@
 from flask import Flask, request, jsonify, Blueprint
 from flask_jwt_extended import jwt_required
+from gates.community.community_route import community_route
+
+gate_to_community_bl = Blueprint("gate_to_community", __name__)
+
+@gate_to_community_bl.route('/find_community', methods=["GET"])
+def find():
+    pass
 
