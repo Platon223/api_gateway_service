d8e2a17946b8f2791127960236a91b6303e7cc31
Platon <platon.tikhnenko@gmail.com>
2025-11-17 16:25:17 -0800

    siplified the route logic in the gate to auth

--- blueprints/auth/routes.py
+++ blueprints/auth/routes.py
@@ -4,37 +4,10 @@ from gates.auth.auth_route import auth_route
 
 gate_to_auth_bl = Blueprint('gate_to_auth', __name__)
 
-@gate_to_auth_bl.route('/login', methods=['POST'])
-def login():
-    return auth_route('login?step=first_entry')
+@gate_to_auth_bl.route('/<endpoint>', methods=['POST', 'GET'])
+def route(endpoint):
+    return auth_route(f'{endpoint}')
 
-@gate_to_auth_bl.route('/register', methods=['POST'])
-def register():
-    return auth_route('register')
-
-@gate_to_auth_bl.route('/verify', methods=['POST'])
-def verify():
-    args = request.args.get('type')
-    if not args:
-        return auth_route('verify')
-    return auth_route(f'verify?type={args}')
-
-@gate_to_auth_bl.route('/refresh', methods=['POST'])
-@jwt_required(refresh=True)
-def refresh():
-    return auth_route('refresh')
-
-@gate_to_auth_bl.route('/forgot_password', methods=['POST'])
-def forgot_password():
-    return auth_route('forgot_password')
-
-@gate_to_auth_bl.route('/new_password', methods=['POST'])
-def new_password():
-    return auth_route('new_password')
-
-@gate_to_auth_bl.route('/oauth', methods=['POST'])
-def oauth():
-    return auth_route('oauth')
 
 
 
