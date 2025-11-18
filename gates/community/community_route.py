febde5c43be26cdd29f0cefccf3d2c0716e55642
Platon <platon.tikhnenko@gmail.com>
2025-11-17 17:26:37 -0800

    created community route fetcher that requests to community service

--- gates/community/community_route.py
+++ gates/community/community_route.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, Response
 from dotenv import load_dotenv
 import requests
 import os
@@ -6,6 +6,19 @@ import os
 load_dotenv()
 
 def community_route(route):
-    requests.post(f"{os.getenv("COMMUNITY_SERVICE_URL")}/community/{route}")
-    
+    json_data = request.get_json()
+    req = requests.post(f"{os.getenv("COMMUNITY_SERVICE_URL")}/community/{route}", json=json_data)
+
+    if req.status_code == 200:
+        return Response(
+            req.text,
+            status=req.status_code,
+            content_type=req.headers.get("content-type")
+        )
+    else:
+        return Response(
+            req.text,
+            status=req.status_code
+        )
+
 
